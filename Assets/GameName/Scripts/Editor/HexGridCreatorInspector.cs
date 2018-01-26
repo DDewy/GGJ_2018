502239ca79062754f0bf45e321f52823ec629f11
David Dewhirst <david.dewhirst1996@GMAIL.COM>
2018-01-26 23:27:01 +0000

    Tried to do a Hex, I cried, help me. oh god. Thank god it is now a square. PRAISE GAWD

---
+++ Assets/GameName/Scripts/Editor/HexGridCreatorInspector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HexGridCreator))]
+public class HexGridCreatorInspector : Editor {
+
+    const float HexSpace = 0.425f;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Create Grid"))
+        {
+            CreateGrid();
+        }
+    }
+
+    void CreateGrid()
+    {
+        HexGridCreator creator = (HexGridCreator)target;
+
+        GameObject[][][] tempGrid = new GameObject[(creator.Width * 2) + 1][][];
+        
+        
+
+
+    }
+}
---
+++ Assets/GameName/Scripts/Editor/SquareGridCreatorInspector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SquareGridCreator))]
+public class SquareGridCreatorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Create Grid"))
+        {
+            CreateGrid();
+        }
+    }
+
+    void CreateGrid()
+    {
+        SquareGridCreator creator = (SquareGridCreator)target;
+        //Initalize Grid
+        GameObject[][] TempGrid = new GameObject[creator.Width][];
+
+        for(int i = 0; i < TempGrid.Length; i++)
+        {
+            TempGrid[i] = new GameObject[creator.Height];
+        }
+
+        const float 
+
+        for(int i = 0; i < TempGrid.Length; i++)
+        {
+            for(int p = 0; p < TempGrid[i].Length; p++)
+            {
+                TempGrid[i][p] = Instantiate(creator.SquareRef);
+                TempGrid[i][p].transform.position = (Vector3) (new Vector2Int(i, p));
+            }
+        }
+    }
+
+}
