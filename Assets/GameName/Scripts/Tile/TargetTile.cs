2444461c074c20f0feb68b0a08c6444b5fedf8b7
David Dewhirst <david.dewhirst1996@GMAIL.COM>
2018-01-28 15:28:50 +0000

    Target Tile Changes, so it can see the light

--- Assets/GameName/Scripts/Tile/TargetTile.cs
+++ Assets/GameName/Scripts/Tile/TargetTile.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class TargetTile : BaseTile
 {
     public Color TargetColour;
+    public bool hasLight = false;
+
+    private void Start()
+    {
+        creator.PrePathUpdate += delegate { hasLight = false; };
+    }
+
+    public void HitByLight()
+    {
+        hasLight = true;
+    }
 
     public override void AssignNewTile(Vector2Int arrayPosition, SquareGridCreator creator)
     {
